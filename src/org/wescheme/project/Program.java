266ae80ba9c97f3dd44b9abada0d449de52279dc
Misha Leder <mishal@dhcp-172-26-12-184.nyc.corp.google.com>
2012-12-01 15:21:11 -0500

    second commit

--- src/org/wescheme/project/Program.java
+++ src/org/wescheme/project/Program.java
@@ -350,4 +350,32 @@ public class Program implements Serializable {
 	public List<Program> getBacklinkedPrograms(PersistenceManager pm) {
 		return Queries.getBacklinkedPrograms(pm, this.id);
 	}
+
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Program other = (Program) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
---
+++ war-src/js/wescheme-properties.js
@@ -0,0 +1,6 @@
+
+goog.provide("plt.wescheme.WeSchemeProperties");
+    
+plt.wescheme.WeSchemeProperties.wescheme_server_base = 'http://wescheme-ml.appspot.com';
+plt.wescheme.WeSchemeProperties.compilation_server_url = 'http://balanced-wescheme-compilers-1567676416.us-west-2.elb.amazonaws.com/servlets/standalone.ss';
+plt.wescheme.WeSchemeProperties.compilation_servers = 'http://balanced-wescheme-compilers-1567676416.us-west-2.elb.amazonaws.com/rpc.html http://LoadBalancerEast-1672652775.us-east-1.elb.amazonaws.com/rpc.html';
